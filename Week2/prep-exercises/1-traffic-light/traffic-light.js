200670a4a77e78129722d04e96637c798eb49e3d
hshamsadd <hshamsadd@gmail.com>
2025-05-15 23:25:48 +0200

    Update traffic-light.js

--- Week2/prep-exercises/1-traffic-light/traffic-light.js
+++ Week2/prep-exercises/1-traffic-light/traffic-light.js
@@ -1,39 +1,21 @@
-"use strict";
-/**
- * The `trafficLight` object is now no longer a global variable. Instead,
- * it is defined in function `main()` and passed as a parameter to other
- * functions, as and when needed.
- */
+```javascript
 
 function getCurrentState(trafficLight) {
-  // TODO
-  // Should return the current state (i.e. colour) of the `trafficLight`
-  // object passed as a parameter.
+  return trafficLight.possibleStates[trafficLight.stateIndex];
 }
 
 function getNextStateIndex(trafficLight) {
-  // TODO
-  // Return the index of the next state of the `trafficLight` such that:
-  // - if the color is green, it will turn to orange
-  // - if the color is orange, it will turn to red
-  // - if the color is red, it will turn to green
+  return (trafficLight.stateIndex + 1) % trafficLight.possibleStates.length;
 }
 
-// This function loops for the number of seconds specified by the `secs`
-// parameter and then returns.
-// IMPORTANT: This is not the recommended way to implement 'waiting' in
-// JavaScript. You will learn better ways of doing this when you learn about
-// asynchronous code.
 function waitSync(secs) {
   const start = Date.now();
-  while (Date.now() - start < secs * 1000) {
-    // nothing do to here
-  }
+  while (Date.now() - start < secs * 1000) {}
 }
 
 function main() {
   const trafficLight = {
-    possibleStates: ["green", "orange", "red"],
+    possibleStates: ["🟢", "🟠", "🔴"],
     stateIndex: 0,
   };
 
@@ -41,20 +23,23 @@ function main() {
     const currentState = getCurrentState(trafficLight);
     console.log(cycle, "The traffic light is now", currentState);
 
-    waitSync(1); // Wait a second before going to the next state
+    waitSync(1);
     trafficLight.stateIndex = getNextStateIndex(trafficLight);
   }
 }
 
 main();
+
 /**
  * The output should be:
 
-0 The traffic light is now green
-1 The traffic light is now orange
-2 The traffic light is now red
-3 The traffic light is now green
-4 The traffic light is now orange
-5 The traffic light is now red
+0 The traffic light is now 🟢
+1 The traffic light is now 🟠
+2 The traffic light is now 🔴
+3 The traffic light is now 🟢
+4 The traffic light is now 🟠
+5 The traffic light is now 🔴
 
 */
+
+```
