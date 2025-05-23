b769b9be2730d7c9d7bf0d2d28685101e753f42a
hshamsadd <hshamsadd@gmail.com>
2025-05-23 17:45:06 +0200

    Finished my 1-find-mentors.js exercise

--- Week3/prep-exercises/1-hyf-program/1-find-mentors.js
+++ Week3/prep-exercises/1-hyf-program/1-find-mentors.js
@@ -1,26 +1,67 @@
 import { modules, students, mentors, classes } from "./hyf.js";
 
 /**
- * Tjebbe would like help to get a list of possible mentors for a module.
- * Fill in this function that finds all the mentors that can teach the given module.
+ * We would like to have a list of everyone that is currently participating in a class.
+ * This means the students, but also the mentors that are currently teaching the class.
+ * The students should be self explanatory, but to find the mentors you will need to follow these steps:
+ * - Check what the `currentModule` of the class is
+ * - Find the mentor(s) that are `nowTeaching` that module
  *
- * It should return an array of names. So something like:
- *  ['John', 'Mary']
+ * Should return the list of names and their roles. So something like:
+ *
+ *  [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }]
  */
-const possibleMentorsForModule = (moduleName) => {
+
+const getPeopleOfClass = (className) => {
   // TODO complete this function
+  const peopleInClass = [];
+  for (let student of students) {
+    if (student.class === className) {
+      peopleInClass.push({
+        name: student.name,
+        role: "student",
+      });
+    }
+  }
+
+  const classItem = classes.find((cls) => cls.name === className);
+  const currentModule = classItem?.currentModule;
+
+  for (let mentor of mentors) {
+    if (mentor.nowTeaching && mentor.nowTeaching === currentModule) {
+      peopleInClass.push({
+        name: mentor.name,
+        role: "mentor",
+      });
+    }
+  }
+  return peopleInClass;
 };
 // You can uncomment out this line to try your function
-// console.log(possibleMentorsForModule('using-apis'));
+console.log(getPeopleOfClass("class34"));
 
 /**
- * Tjebbe wants to make it even easier for himself.
- * Fill in this function that chooses a random mentor to teach the given module.
+ * We would like to have a complete overview of the current active classes.
+ * First find the active classes, then for each get the people of that class.
  *
- * It should return a single name.
+ * Should return an object with the class names as properties.
+ * Each class name property contains an array identical to the return from `getPeopleFromClass`. So something like:
+ *
+ *  {
+ *    class34: [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }],
+ *    class35: [{ name: 'Jane', role: 'student' }, { name: 'Steve', role: 'mentor' }]
+ *  }
  */
-const findMentorForModule = (moduleName) => {
+const getActiveClasses = () => {
   // TODO complete this function
+  const activeClasses = {};
+  for (let cls of classes) {
+    if (cls.active === true) {
+      const peopleList = getPeopleOfClass(cls.name);
+      activeClasses[cls.name] = peopleList;
+    }
+  }
+  return activeClasses;
 };
 // You can uncomment out this line to try your function
-// console.log(findMentorForModule('javascript'));
+console.log(getActiveClasses());
